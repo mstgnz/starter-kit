6e38a7091a99c75116a88d1228b45b079a02477e
Mesut GENEZ <genezmesut@gmail.com>
2024-08-11 00:27:55 +0300

    auth

--- pkg/auth/bcrypt.go
+++ pkg/auth/bcrypt.go
@@ -4,13 +4,11 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type Bcrypt struct{}
-
-func (b *Bcrypt) HashAndSalt(password string) string {
+func HashAndSalt(password string) string {
 	hash, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
 	return string(hash)
 }
 
-func (b *Bcrypt) ComparePassword(hashedPassword, password string) bool {
+func ComparePassword(hashedPassword, password string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) == nil
 }
